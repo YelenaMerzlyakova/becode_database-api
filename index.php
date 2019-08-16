22f4c966c9810868b148d02dc6aea26044178f9c
YelenaMerzlyakova <merzlyakova.lena@gmail.com>
2019-08-16 14:22:51 +0200

    front api

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
+</head>
+<body>
+	<form method="POST" action="notes.php" >
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" value="">
+		</div>
+		<div class="input-group">
+			<button class="btn" type="submit" name="submit" >Submit</button>
+		</div>
+	</form>
+</body>
+</html>
